6500e60b495e9385576f49eb13deeb2a7cf68f6d
Lorenzo <lescudero@miners.utep.edu>
2024-03-12 22:14:45 -0600

    Added one hot encoding code for category section in decisionTreeModel.py

--- decisionTreeModel.py
+++ decisionTreeModel.py
@@ -1,13 +1,11 @@
 import pandas as pd
 
-
 df = pd.read_csv('./archive/fraudTrain_simple.csv')
-
 # This line was uncommented because fraudTrain.csv is too big, fraudTrain_simple.csv is being used in the mean time
 # df = pd.read_csv('./archive/fraudTrain.csv')
 
 # Preprocessing the dataset
-# trans_date_trans_time needs to be broken down:
+# trans_date_trans_time needs to be broken down to be taken into consideration for numerical data
 df['trans_date_trans_time'] = pd.to_datetime(df['trans_date_trans_time'], format = '%m/%d/%y %H:%M')
 
 df['hour'] = df['trans_date_trans_time'].dt.hour
@@ -15,3 +13,10 @@ df['day'] = df['trans_date_trans_time'].dt.day
 df['month'] = df['trans_date_trans_time'].dt.month
 df['year'] = df['trans_date_trans_time'].dt.year
 df['dayofweek'] = df['trans_date_trans_time'].dt.dayofweek
+
+# One hot encoding on 'category' column:
+catergories = pd.get_dummies(df['category'], prefix = 'category')
+
+# Concatenating one hot encoding data frame
+df = pd.concat([df, catergories], axis = 1)
+df.drop(['category', 'trans_date_trans_time'], axis = 1, inplace = True)
